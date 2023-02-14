44616afb1197c02959d8f518c2867125882c2445
Luis Leal <luisrrleal@gmail.com>
2023-02-14 09:47:34 -0600

    Add image

--- src/main/java/com/example/bmicalculator/BMICalculator.java
+++ src/main/java/com/example/bmicalculator/BMICalculator.java
@@ -5,9 +5,12 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -21,8 +24,8 @@ public class BMICalculator extends Application {
     TextField tf_height = new TextField();
     Label l_showResult = new Label();
 
-    Label l_message = new Label();
 
+    //Label l_message = new Label();
     Button b_cancel = new Button("Cancel");
     Button b_calculate = new Button("Calculate");
 
@@ -30,6 +33,7 @@ public class BMICalculator extends Application {
     public void start(Stage stage) throws IOException {
         StackPane root = initGUI();
         root.setAlignment(Pos.CENTER);
+
 
 
         //Show screen
@@ -46,6 +50,13 @@ public class BMICalculator extends Application {
         //Create all elements (not in level class because it is only reference, were not using them)
         Label l_title = new Label("BMI Calculator");
         l_title.setAlignment(Pos.CENTER);
+
+        //Declare and instance image
+        Image img_img = new Image(getClass().getResource("/images/1.jpg").toString());
+        //Generate image
+        ImageView imgview_img = new ImageView(img_img);
+        imgview_img.setFitHeight(100);
+        imgview_img.setFitWidth(200);
 
         Label l_weight = new Label("Weight");
         Label l_height = new Label("Height");
@@ -67,10 +78,13 @@ public class BMICalculator extends Application {
         gridpane.add(l_result,0,3);
         gridpane.add(l_showResult, 1,3);
 
-        gridpane.add(l_message,0,4,2,1);
+
+        //gridpane.add(l_message,0,4,2,1);
 
         gridpane.add(b_cancel, 0, 5);
         gridpane.add(b_calculate, 1, 5);
+
+        gridpane.add(imgview_img, 0,6,2,2);
 
         //Add gridpane to stackpane
         stackPane.getChildren().add(gridpane);
@@ -98,7 +112,8 @@ public class BMICalculator extends Application {
             l_showResult.setText(result+"");
 
             String message = getBMICategory(result);
-            l_message.setText("You're in the " + message);
+            showMessage("BMI Category", message);
+           /* l_message.setText("You're in the " + message);*/
         }
     };
 
@@ -111,6 +126,13 @@ public class BMICalculator extends Application {
         else message="the obese range.";
 
         return message;
+    }
+
+    private void showMessage(String title, String message){
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle(title);
+        alert.setContentText(message);
+        alert.show();
     }
 
     public static void main(String[] args) {
